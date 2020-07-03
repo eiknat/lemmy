96f2bc17eff58a2e8ed0b2f71266a3723fa4bfbe
GreatBearShark <greatbearshark@protonmail.com>
2020-07-02 21:04:07 -0500

    Add 404 page

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -100,6 +100,12 @@ class Index extends Component<any, any> {
                   path={`/password_change/:token`}
                   component={PasswordChange}
                 />
+                <Route path="*">
+                  <img
+                    src="https://cdn.discordapp.com/attachments/727577169370415295/728422229246869514/glitchbear.png"
+                    className="img-fluid"
+                  />
+                </Route>
               </Switch>
               <Symbols />
             </div>
