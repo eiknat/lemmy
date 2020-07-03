2635176fea3f1c2572e8855b73fb71644fd4f038
GreatBearShark <greatbearshark@protonmail.com>
2020-07-03 13:14:35 -0500

    Remove duplicate style

--- ui/src/components/post-listing.tsx
+++ ui/src/components/post-listing.tsx
@@ -143,7 +143,7 @@ export class PostListing extends Component<PostListingProps, PostListingState> {
                 <pre>{this.props.post.body}</pre>
               ) : (
                 <div
-                  className="md-div border-secondary post-listing-body"
+                  className="md-div post-listing-body"
                   dangerouslySetInnerHTML={mdToHtml(this.props.post.body)}
                 />
               )}
